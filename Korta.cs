6bb19ee25bd5cb88c9d90b4a0dd4abc99e3e9680
Edgaras1 <kurfreeze@gmail.com>
2016-12-01 18:12:12 +0200

    Pridetas kortu tipas

--- Korta.cs
+++ Korta.cs
@@ -4,9 +4,23 @@ namespace cardgame
 {
 	public class Korta
 	{
-		public Korta ()
+
+		public Korta ( KortosTipas tipas, int verte )
 		{
+			this.tipas = tipas;
+			this.verte = verte;
 		}
+
+		public KortosTipas tipas { get; private set; }
+		public int verte { get; private set; }
+	}
+
+	public enum KortosTipas
+	{
+		Pikai,
+		Sirdys,
+		Bugnai,
+		Kryziai
 	}
 }
 
