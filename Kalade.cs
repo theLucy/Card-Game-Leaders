98f9d76a42df034c165d7a00cee028d0d4e00157
Eivydas1 <eivydassz1@gmail.com>
2016-12-13 22:46:09 +0200

    pridejau 3 ir kalade +

--- Kalade.cs
+++ Kalade.cs
@@ -2,11 +2,40 @@
 
 namespace cardgame
 {
-	public class Kalade
-	{
-		public Kalade ()
-		{
-		}
-	}
+    public class Kalade
+    {
+        public List<Korta> Kortos = new List<Korta>();
+        public Kalade()
+        {
+            kurti_kortas();
+            maisyti();
+
+        }
+        void kurti_kortas()
+        {
+            for (int i = 2; i < 15; i++)
+            {
+                Kortos.Add(new Korta(KortosTipas.Pikai, i));
+                Kortos.Add(new Korta(KortosTipas.Kryziai, i));
+                Kortos.Add(new Korta(KortosTipas.Bugnai, i));
+                Kortos.Add(new Korta(KortosTipas.Sirdys, i));
+                //2-10 skaiciai, J-11, Q-12, K-13, A-14
+            }
+        }
+        public void maisyti()
+        {
+            Random rnd = new Random();
+            int n = Kortos.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rnd.Next(n + 1);
+                Korta v = Kortos[k];
+                Kortos[k] = Kortos[n];
+                Kortos[n] = v;
+            }
+        }
+
+    }
 }
 
